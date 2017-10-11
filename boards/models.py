c9340d9530ae133127ecb4b76847ee866f8cc55c
David Gengler <davidgengler@gmail.com>
2017-10-11 15:35:39 -0400

    working on displays

--- boards/models.py
+++ boards/models.py
@@ -3,15 +3,20 @@ from django.db import models
 # Create your models here.
 
 from django.contrib.auth.models import User
-
+from django.utils.text import Truncator
 
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
-	
-	# Add the following so you can see what the board names are specifically instead of just 'Board object' when running Board.objects.all()
+
     def __str__(self):
         return self.name
+
+    def get_posts_count(self):
+        return Post.objects.filter(topic__board=self).count()
+
+    def get_last_post(self):
+        return Post.objects.filter(topic__board=self).order_by('-created_at').first()
 
 
 class Topic(models.Model):
@@ -20,6 +25,8 @@ class Topic(models.Model):
     board = models.ForeignKey(Board, related_name='topics')
     starter = models.ForeignKey(User, related_name='topics')
     last_updated = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+      return self.subject
 
 
 class Post(models.Model):
@@ -29,3 +36,6 @@ class Post(models.Model):
     updated_at = models.DateTimeField(null=True)
     created_by = models.ForeignKey(User, related_name='posts')
     updated_by = models.ForeignKey(User, null=True, related_name='+')
+    def __str__(self):
+        truncated_message = Truncator(self.message)
+        return truncated_message.chars(30)
--- boards/views.py
+++ boards/views.py
@@ -3,7 +3,7 @@
 # Create your views here.
 
 from django.contrib.auth.models import User
-from .forms import NewTopicForm
+from .forms import NewTopicForm, PostForm
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Board, Topic, Post
@@ -36,7 +36,7 @@ def new_topic(request, pk):
                 topic=topic,
                 created_by=request.user  # <- and here
             )
-            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+            return redirect('topic_posts', pk=pk, topic_pk=topic.pk)  # redirect to the created topic page
     else:
         form = NewTopicForm()
     return render(request, 'new_topic.html', {'board': board, 'form': form})
@@ -44,6 +44,21 @@ def new_topic(request, pk):
 def topic_posts(request, pk, topic_pk):
     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
     return render(request, 'topic_posts.html', {'topic': topic})
+	
+@login_required
+def reply_topic(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.topic = topic
+            post.created_by = request.user
+            post.save()
+            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+    else:
+        form = PostForm()
+    return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
 	
 # def about(request):
     # # do something...
